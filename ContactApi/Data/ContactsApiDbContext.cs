cbafe6fadb9e154e5bf8a8d935346d3042a9eafc
canyoufindA <anurudhrajput93@gmail.com>
2023-03-13 04:30:55 +0530

    Add project files.

---
+++ ContactApi/Data/ContactsApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactApi.Data
+{
+    public class ContactsApiDbContext : DbContext
+    {
+        public ContactsApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Models.Contact> Contacts { get; set; }
+    }
+}
---
+++ ContactApi/Models/AddContactRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactApi.Models
+{
+    public class AddContactRequest
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
